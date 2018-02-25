0c48501f7ceb0546b1ed61bbc040a395e5e92fc6
Patrick Hensley <pathensley@gmail.com>
2018-02-25 17:27:29 -0500

    Increase code coverage of cldr-compiler.

---
+++ packages/cldr-compiler/__tests__/resource/machine.test.ts
@@ -0,0 +1,105 @@
+import {
+  Choice,
+  Origin,
+  Scope,
+  digits,
+  field,
+  fieldmap,
+  objectmap,
+  origin,
+  scope,
+  scopefield,
+  scopemap
+} from '@phensley/cldr-schema';
+
+import { Locale, LanguageResolver } from '@phensley/cldr-core';
+import { Encoder, EncoderMachine } from '../../src/resource/machine';
+import { ResourcePack } from '../../src/resource/pack';
+
+const parseLocale = (id: string) => ({ id, tag: LanguageResolver.resolve(id) });
+
+const EN_US = parseLocale('en');
+const EN_DE = parseLocale('en-DE');
+const EN_CA = parseLocale('en-CA');
+
+const NumberSymbolValues = ['decimal', 'group'];
+
+const NUMBERS: Scope = scope('Numbers', 'Numbers', [
+  objectmap('symbols', NumberSymbolValues),
+
+  scope('currencyFormats', 'currencyFormats', [
+    field('standard', 'standard')
+  ])
+]);
+
+const ORIGIN: Origin = origin([
+  NUMBERS
+]);
+
+const SOURCE_EN_US = {
+  Numbers: {
+    currencyFormats: {
+      standard: '¤#,##0.00'
+    },
+    symbols: {
+      decimal: '.',
+      group: ','
+    },
+  }
+};
+
+const SOURCE_EN_DE = {
+  Numbers: {
+    currencyFormats: {
+      standard: '#,##0.00 ¤'
+    },
+    symbols: {
+      decimal: ',',
+      group: '.'
+    }
+  }
+};
+
+const SOURCE_EN_CA = {
+  Numbers: {
+    currencyFormats: {
+      standard: '¤#,##0.00'
+    },
+    symbols: {
+      decimal: '.',
+      group: ','
+    }
+  }
+};
+
+class PackEncoder implements Encoder {
+
+  constructor(private pack: ResourcePack) { }
+
+  encode(f: string | undefined): number {
+    return this.pack.add(f === undefined ? '' : f);
+  }
+}
+
+test('encoding', () => {
+  const pack = new ResourcePack('en', '0.1.0', '32.0.1');
+  const encoder = new PackEncoder(pack);
+  const machine = new EncoderMachine(encoder);
+
+  pack.push(EN_US);
+  machine.encode(SOURCE_EN_US, ORIGIN);
+
+  pack.push(EN_DE);
+  machine.encode(SOURCE_EN_DE, ORIGIN);
+
+  pack.push(EN_CA);
+  machine.encode(SOURCE_EN_CA, ORIGIN);
+
+  const raw = pack.render();
+  const p = JSON.parse(raw);
+
+  expect(Object.keys(p.scripts)).toEqual(['Latn']);
+  const { strings, exceptions, regions } = p.scripts.Latn;
+  expect(strings).toEqual('.\t,\t¤#,##0.00');
+  expect(exceptions).toEqual(',\t.\t#,##0.00 ¤');
+});
---
+++ packages/cldr-compiler/__tests__/resource/pack.test.ts
@@ -0,0 +1,62 @@
+import { ResourcePack } from '../../src/resource/pack';
+import { Locale, LanguageResolver, encoding } from '@phensley/cldr-core';
+
+const { base100encode, base100decode } = encoding;
+const parseLocale = (id: string) => ({ id, tag: LanguageResolver.resolve(id) });
+
+const EN_US = parseLocale('en');
+const EN_CA = parseLocale('en-CA');
+const EN_GB = parseLocale('en-GB');
+const EN_DE = parseLocale('en-DE');
+
+test('creation', () => {
+  const pack = new ResourcePack('en', '0.1.0', '32.0.1');
+
+  pack.push(EN_US);
+  pack.add('foo');
+  pack.add('bar');
+  pack.add('baz');
+  pack.add('quux');
+
+  pack.push(EN_GB);
+  pack.add('foo');
+  pack.add('bar.gb');
+  pack.add('baz');
+  pack.add('quux.gb');
+
+  pack.push(EN_CA);
+  pack.add('foo');
+  pack.add('bar');
+  pack.add('baz');
+  pack.add('quux.ca');
+
+  pack.push(EN_DE);
+  pack.add('foo.de');
+  pack.add('bar.de');
+  pack.add('baz');
+  pack.add('quux.de');
+
+  const raw = pack.render();
+  const p = JSON.parse(raw);
+  expect(p.language).toEqual('en');
+  expect(p.version).toEqual('0.1.0');
+  expect(p.cldr).toEqual('32.0.1');
+
+  expect(Object.keys(p.scripts)).toEqual(['Latn']);
+
+  const { strings, exceptions, regions } = p.scripts.Latn;
+  expect(strings).toEqual('foo\tbar\tbaz\tquux');
+  expect(exceptions).toEqual('bar.gb\tquux.gb\tquux.ca\tfoo.de\tbar.de\tquux.de');
+
+  // US is the base since it has the minimum distance to the other regions.
+  expect(regions.US).toEqual('');
+
+  // GB exception index:  {1: 0, 3: 1}
+  expect(regions.GB).toEqual([1, 0, 3, 1].map(base100encode).join(' '));
+
+  // CA exception index: {3: 2}
+  expect(regions.CA).toEqual([3, 2].map(base100encode).join(' '));
+
+  // DE exception index: {0: 3, 1: 4, 3: 5}
+  expect(regions.DE).toEqual([0, 3, 1, 4, 3, 5].map(base100encode).join(' '));
+});
