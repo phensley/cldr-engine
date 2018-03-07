e92b58a8ce274a9a579a3803787f6aca5c45d7b2
Patrick Hensley <pathensley@gmail.com>
2018-03-07 14:44:40 -0500

    Sketched out basics of Rational type, added tests.

--- packages/cldr-core/__tests__/types/numbers/rational.test.ts
+++ packages/cldr-core/__tests__/types/numbers/rational.test.ts
@@ -1,7 +1,43 @@
-import { Rational } from '../../../src/types/numbers';
+import { Decimal, DecimalArg, DecimalConstants, Rational } from '../../../src/types/numbers';
 
-test('basics', () => {
-  // TODO:
-  console.log(Rational.parse('1 / pi'));
-  console.log(Rational.parse('1/e'));
+const rat = (a: DecimalArg, b: DecimalArg): Rational => new Rational(a, b);
+const parse = (s: string) => Rational.parse(s);
+
+test('parsing', () => {
+  let r = Rational.parse('1 / pi');
+  expect(r.numerator).toEqual(DecimalConstants.ONE);
+  expect(r.denominator).toBe(DecimalConstants.PI);
+
+  r = Rational.parse('1 / e');
+  expect(r.numerator).toEqual(DecimalConstants.ONE);
+  expect(r.denominator).toBe(DecimalConstants.E);
+
+  r = Rational.parse('-17.35e-1');
+  expect(r.numerator).toEqual(new Decimal('-17.35e-1'));
+
+  r = rat('1.5', '2.3');
+  expect(r.numerator).toEqual(new Decimal('1.5'));
+  expect(r.denominator).toEqual(new Decimal('2.3'));
+
+  r = new Rational(17);
+  expect(r.numerator).toEqual(new Decimal('17'));
+  expect(r.denominator).toEqual(DecimalConstants.ONE);
+});
+
+test('multiplication', () => {
+  expect(parse('1 / 5').multiply('2 / 7')).toEqual(parse('2 / 35'));
+});
+
+test('divide', () => {
+  expect(parse('1 / 5').divide('2 / 7')).toEqual(parse('7 / 10'));
+});
+
+test('inverse', () => {
+  expect(parse('1 / 5').inverse()).toEqual(parse('5 / 1'));
+});
+
+test('compare', () => {
+  expect(parse('1 / 5').compare(parse('1 / 5'))).toEqual(0);
+  expect(parse('1 / 5').compare(parse('1 / 6'))).toEqual(1);
+  expect(parse('.1 / 10').compare(parse('.2 / 10'))).toEqual(-1);
 });
--- packages/cldr-core/src/types/numbers/rational.ts
+++ packages/cldr-core/src/types/numbers/rational.ts
@@ -1,9 +1,30 @@
-import { Decimal, DecimalConstants } from './decimal';
+import { Decimal, DecimalArg, DecimalConstants } from './decimal';
+import { MathContext } from './types';
+
+const coerceDecimal = (n: DecimalArg): Decimal => {
+  if (typeof n === 'number') {
+    return new Decimal(n);
+  } else if (typeof n === 'string') {
+    return fromString(n);
+  }
+  return n;
+};
+
+const coerceRational = (n: RationalArg): Rational => {
+  if (typeof n === 'number') {
+    return new Rational(n, DecimalConstants.ONE);
+  } else if (typeof n === 'string') {
+    return Rational.parse(n);
+  } else if (n instanceof Rational || (n as any).numerator !== undefined) {
+    return n as Rational;
+  }
+  return new Rational(n, DecimalConstants.ONE);
+};
 
 /**
  * Default Rational parser, to identify named constants.
  */
-const parse = (s: string): Decimal => {
+const fromString = (s: string): Decimal => {
   switch (s.toLowerCase()) {
   case 'e':
     return DecimalConstants.E;
@@ -14,6 +35,8 @@ const parse = (s: string): Decimal => {
   }
 };
 
+export type RationalArg = Rational | Decimal | number | string;
+
 /**
  * Arbitrary precision rational type.
  *
@@ -21,18 +44,59 @@ const parse = (s: string): Decimal => {
  */
 export class Rational {
 
+  readonly numerator: Decimal;
+  readonly denominator: Decimal;
+
   constructor(
-    private numerator: string | Decimal,
-    private denominator: string | Decimal) { }
+    numerator: DecimalArg,
+    denominator: DecimalArg = DecimalConstants.ONE) {
+
+    this.numerator = coerceDecimal(numerator);
+    this.denominator = coerceDecimal(denominator);
+  }
 
   static parse(raw: string): Rational {
     const i = raw.indexOf('/');
     if (i === -1) {
-      throw new Error('Rational must be of the form n/d');
+      return new Rational(raw, DecimalConstants.ONE);
     }
     const numerator = raw.substring(0, i).trim();
     const denominator = raw.substring(i + 1).trim();
-    return new Rational(parse(numerator), parse(denominator));
+    return new Rational(numerator, denominator);
+  }
+
+  compare(num: RationalArg, context?: MathContext): number {
+    const n = coerceRational(num);
+    const a = this.numerator.multiply(n.denominator, context);
+    const b = n.numerator.multiply(this.denominator, context);
+    return a.compare(b);
+  }
+
+  divide(num: RationalArg, context?: MathContext): Rational {
+    const n = coerceRational(num);
+    return new Rational(
+      this.numerator.multiply(n.denominator, context),
+      this.denominator.multiply(n.numerator, context)
+    );
+  }
+
+  multiply(num: RationalArg, context?: MathContext): Rational {
+    const n = coerceRational(num);
+    return new Rational(
+      this.numerator.multiply(n.numerator, context),
+      this.denominator.multiply(n.denominator, context)
+    );
+  }
+
+  inverse(): Rational {
+    return new Rational(this.denominator, this.numerator);
+  }
+
+  toDecimal(context?: MathContext): Decimal {
+    if (context === undefined) {
+      context = { precision: this.numerator.precision() + this.denominator.precision() };
+    }
+    return this.numerator.divide(this.denominator, context);
   }
 
 }
