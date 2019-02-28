957f71ac2c68ee1885710511c1846a7fdd20f13a
Patrick Hensley <pathensley@gmail.com>
2019-02-28 07:16:44 -0500

    Initial cldr package rollup config

---
+++ packages/cldr/rollup.config.ts
@@ -0,0 +1,61 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+// import { dts, ts } from 'rollup-plugin-dts';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import { uglify } from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
+
+const libraryName = 'cldrengine';
+
+const config = [
+  {
+    input: `src/cldr.ts`,
+    output: [
+      {
+        file: `dist/${libraryName}.umd.js`,
+        name: libraryName,
+        format: 'umd',
+        sourcemap: true
+      }
+    ],
+    external: [],
+    watch: {
+      include: 'src/**',
+    },
+    plugins: [
+      json({
+        compact: true
+      }),
+      resolve({
+        jsnext: true,
+        extensions: ['.ts'],
+      }),
+      typescript({
+        // verbosity: 3,
+        tsconfig: 'tsconfig.build.json',
+        tsconfigDefaults: {
+          compilerOptions: {
+            declaration: false,
+            declarationMap: false,
+            resolveJsonModule: true,
+            module: 'esnext',
+            types: [
+              '@phensley/cldr-core',
+              '@phensley/cldr-schema',
+              '@phensley/cldr-utils',
+              '@phensley/decimal',
+              'node'
+            ]
+          }
+        }
+      }),
+      commonjs(),
+      sourceMaps(),
+      uglify({}, minify)
+    ],
+  },
+];
+
+export default config;
