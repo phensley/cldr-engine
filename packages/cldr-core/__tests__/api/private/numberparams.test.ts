8f6e5b9d670628358767cc6d6af57567c48ac842
Patrick Hensley <pathensley@gmail.com>
2019-03-19 10:39:32 -0400

    Coverage for calendar patterns and number params

---
+++ packages/cldr-core/__tests__/api/private/numberparams.test.ts
@@ -0,0 +1,38 @@
+import { languageBundle } from '../../_helpers';
+import { InternalsImpl } from '../../../src/internals';
+import { NumberParams } from '../../../src/common/private';
+import { NumberParamsCache } from '../../../src/api/private';
+
+const LATN_DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const INTERNALS = new InternalsImpl();
+
+const getCache = (tag: string) =>
+  new NumberParamsCache(languageBundle(tag), INTERNALS);
+
+test('number systems', () => {
+  const bundle = languageBundle('zh');
+  const res = INTERNALS.schema.Numbers.numberSystems.mapping(bundle);
+  expect(res.finance).toEqual('hansfin');
+});
+
+test('number params cache', () => {
+  let cache: NumberParamsCache;
+  let params: NumberParams;
+
+  cache = getCache('en');
+  params = cache.getNumberParams('finance');
+  expect(params.digits).toEqual(LATN_DIGITS);
+  expect(params.primaryGroupingSize).toEqual(3);
+
+  // NOTE: hansfin not implemented, so falls back to 'latn'
+  cache = getCache('zh');
+  params = cache.getNumberParams('finance');
+  expect(params.digits).toEqual(LATN_DIGITS);
+  expect(params.primaryGroupingSize).toEqual(3);
+
+  cache = getCache('fa-AF');
+  params = cache.getNumberParams('finance');
+  expect(params.digits).toEqual(
+    [ '۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹' ]);
+  expect(params.primaryGroupingSize).toEqual(3);
+});
---
+++ packages/cldr-core/__tests__/internals/calendars/patterns.test.ts
@@ -0,0 +1,35 @@
+import { languageBundle } from '../../_helpers';
+
+import {
+  Bundle,
+  CalendarsImpl,
+  GregorianDate,
+  InternalsImpl,
+  PrivateApiImpl
+} from '../../../src';
+
+import { CalendarPatterns } from '../../../src/internals/calendars/patterns';
+
+// March 11, 2018 7:00:25 AM UTC
+const MARCH_11_2018_070025_UTC = 1520751625000;
+
+const INTERNALS = new InternalsImpl();
+
+const privateApi = (bundle: Bundle) => new PrivateApiImpl(bundle, INTERNALS);
+
+test('calendar patterns', () => {
+  const bundle = languageBundle('en');
+  const api = new CalendarsImpl(bundle, INTERNALS, privateApi(bundle));
+
+  const { schema } = INTERNALS;
+  const impl = new CalendarPatterns(bundle, INTERNALS, schema.Buddhist);
+
+  const date = api.toGregorianDate({ date: MARCH_11_2018_070025_UTC, zoneId: 'America/New_York' });
+  const skel = impl.parseSkeleton('y');
+  const pattern = impl.getAvailablePattern(date, skel);
+  expect(pattern).toEqual([
+    ['y', 1],
+    ' ',
+    ['G', 1]
+  ]);
+});
