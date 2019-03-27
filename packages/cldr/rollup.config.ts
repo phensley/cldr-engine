f525e5c51e58808e0aedffb31495a64bfd1000db
Patrick Hensley <pathensley@gmail.com>
2019-03-27 16:05:08 -0400

    Build rollup from transpiled javascript

--- packages/cldr/rollup.config.ts
+++ packages/cldr/rollup.config.ts
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 // import { dts, ts } from 'rollup-plugin-dts';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-import typescript from 'rollup-plugin-typescript2';
+// import typescript from 'rollup-plugin-typescript2';
 import { uglify } from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
 
@@ -11,7 +11,7 @@ const libraryName = 'cldrengine';
 
 const config = [
   {
-    input: `src/cldr.ts`,
+    input: `lib/cldr.js`,
     output: [
       {
         file: `dist/${libraryName}.umd.js`,
@@ -30,28 +30,31 @@ const config = [
       }),
       resolve({
         jsnext: true,
-        extensions: ['.ts'],
+        extensions: ['.js', '.json'],
       }),
-      typescript({
-        // verbosity: 3,
-        tsconfig: 'tsconfig.build.json',
-        tsconfigDefaults: {
-          compilerOptions: {
-            declaration: false,
-            declarationMap: false,
-            resolveJsonModule: true,
-            module: 'esnext',
-            types: [
-              '@phensley/cldr-core',
-              '@phensley/cldr-schema',
-              '@phensley/cldr-utils',
-              '@phensley/decimal',
-              'node'
-            ]
-          }
-        }
+      // typescript({
+      //   // verbosity: 3,
+      //   tsconfig: 'tsconfig.build.json',
+      //   tsconfigDefaults: {
+      //     compilerOptions: {
+      //       declaration: false,
+      //       declarationMap: false,
+      //       resolveJsonModule: true,
+      //       module: 'esnext',
+      //       types: [
+      //         '@phensley/cldr-core',
+      //         '@phensley/cldr-schema',
+      //         '@phensley/cldr-utils',
+      //         '@phensley/decimal',
+      //         '@phensley/timezone',
+      //         'node'
+      //       ]
+      //     }
+      //   }
+      // }),
+      commonjs({
+        // extensions: ['.js', '.json']
       }),
-      commonjs(),
       sourceMaps(),
       uglify({}, minify)
     ],
