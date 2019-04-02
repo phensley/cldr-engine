73440a0258356eab223e2567062d1849664f26ea
Patrick Hensley <pathensley@gmail.com>
2019-04-02 15:25:59 -0400

    Remove code duplication in gregorian schema

--- packages/cldr-schema/src/code/gregorian.ts
+++ packages/cldr-schema/src/code/gregorian.ts
@@ -1,5 +1,3 @@
-import { field, scope, vector1, vector2, Scope } from '../types';
-
 import {
   GregorianAvailableFormatIndex,
   GregorianEraIndex,
@@ -7,24 +5,9 @@ import {
   GregorianMonthsIndex
 } from '../schema';
 
-const formats = (name: string, rename: string) => scope(name, rename, [
-  vector2('weekdays', 'field-width', 'weekday'),
-  vector2('months', 'field-width', 'gregorian-month'),
-  vector2('quarters', 'field-width', 'quarter'),
-  vector2('dayPeriods', 'field-width', 'day-period'),
-]);
+import { calendarScope } from './common';
 
-export const GREGORIAN: Scope = scope('Gregorian', 'Gregorian', [
-  vector2('eras', 'era-type', 'gregorian-era'),
-  formats('format', 'format'),
-  formats('standAlone', 'standAlone'),
-  vector2('availableFormats', 'plural-key', 'gregorian-available-format'),
-  vector2('intervalFormats', 'date-time-pattern-field', 'gregorian-interval-format'),
-  vector1('dateFormats', 'format-width'),
-  vector1('timeFormats', 'format-width'),
-  vector1('dateTimeFormats', 'format-width'),
-  field('intervalFormatFallback')
-]);
+export const GREGORIAN = calendarScope('gregorian', 'Gregorian');
 
 export const GREGORIAN_INDICES = {
   'gregorian-available-format': GregorianAvailableFormatIndex,
