cd2ac95c28447af1c597aa0ec61395a78c119872
Patrick Hensley <pathensley@gmail.com>
2019-04-11 13:40:57 -0400

    Remove unused utf-8 encoder

--- packages/cldr-utils/__benchmarks__/index.ts
+++ packages/cldr-utils/__benchmarks__/index.ts
@@ -1,6 +1,5 @@
 import { fnv1aSuite } from './fnv1a';
 import { lruSuite, mapSuite } from './lru';
-import { utfSuite } from './utf';
 import { vuintSuite } from './vuint';
 
 const options = { async: false, delay: 0.5 };
@@ -8,5 +7,4 @@ const options = { async: false, delay: 0.5 };
 fnv1aSuite.run(options);
 lruSuite.run(options);
 mapSuite.run(options);
-utfSuite.run(options);
 vuintSuite.run(options);
