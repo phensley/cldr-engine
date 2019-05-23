eb3ef1d661a42c7bda816b2c89c2396f695d11f1
Patrick Hensley <pathensley@gmail.com>
2019-05-23 16:24:46 -0400

    Remove z85/vuint encoders/decoders and tests

--- packages/cldr-utils/__benchmarks__/index.ts
+++ packages/cldr-utils/__benchmarks__/index.ts
@@ -1,10 +1,8 @@
 import { fnv1aSuite } from './fnv1a';
 import { lruSuite, mapSuite } from './lru';
-import { vuintSuite } from './vuint';
 
 const options = { async: false, delay: 0.5 };
 
 fnv1aSuite.run(options);
 lruSuite.run(options);
 mapSuite.run(options);
-vuintSuite.run(options);
--- packages/cldr-utils/src/index.ts
+++ packages/cldr-utils/src/index.ts
@@ -1,6 +1,4 @@
 export * from './cache';
 export * from './checksum';
-export * from './decoding';
-export * from './encoding';
 export * from './lru';
 export * from './search';
