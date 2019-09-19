203952be4b1151a581e6577cba002278fee61d43
Patrick Hensley <pathensley@gmail.com>
2019-09-19 19:39:33 -0400

    Add benchmark of relative time formatting

--- packages/cldr-core/__benchmarks__/calendars/calendars.ts
+++ packages/cldr-core/__benchmarks__/calendars/calendars.ts
@@ -3,7 +3,8 @@
 import { Suite } from 'benchmark';
 import { makeSuite } from '../../../cldr-utils/__benchmarks__/util';
 import { calendarsApi } from '../../__tests__/_helpers';
-import { CalendarsImpl, DateFormatOptions, GregorianDate } from '../../src';
+import { CalendarsImpl, DateFormatOptions, GregorianDate, TimePeriod } from '../../src';
+import { RelativeTimeFormatOptions } from '../../lib';
 
 const BUNDLES: { [x: string]: CalendarsImpl} = {
   'en': calendarsApi('en'),
@@ -33,6 +34,18 @@ const EPOCHS = [
   -23811152979000
 ];
 
+const DATES = [
+  GregorianDate.fromUnixEpoch(952761625000, 'UTC', 1, 1),
+  GregorianDate.fromUnixEpoch(969019200000, 'UTC', 1, 1),
+  GregorianDate.fromUnixEpoch(967809600000, 'UTC', 1, 1),
+];
+
+const INCR: TimePeriod[] = [
+  { day: 10, hour: 7 },
+  { month: -3, day: -3 },
+  { year: 0.5, month: 3, day: 82 }
+];
+
 ZONES.forEach(z => {
   EPOCHS.forEach(e => {
     gregorianSuite.add(`construct ${e} ${z}`, () => {
@@ -50,6 +63,15 @@ Object.keys(BUNDLES).forEach(k => {
       engine.formatDate(date, opts);
       formatDateSuite.add(`format ${k} ${e} ${z}`, () => {
         engine.formatDate(date, opts);
+      });
+    });
+  });
+  DATES.forEach(d => {
+    INCR.forEach(i => {
+      const opts: RelativeTimeFormatOptions = { dayOfWeek: true };
+      const engine = BUNDLES[k];
+      formatDateSuite.add(`relative time ${k} ${d.unixEpoch()} ${JSON.stringify(i)}`, () => {
+        engine.formatRelativeTime(d, d.add(i), opts);
       });
     });
   });
