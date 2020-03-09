c8ddb7d3839663d34d9b7bd4debfab0732123908
Patrick Hensley <pathensley@gmail.com>
2020-03-09 15:43:08 -0400

    Filter grandfathered tags from language alias map
    These are substituted when language tags are parsed.

--- packages/cldr-compiler/src/cli/generator/code/aliases.ts
+++ packages/cldr-compiler/src/cli/generator/code/aliases.ts
@@ -1,11 +1,49 @@
+import * as fs from 'fs';
+import { join } from 'path';
+import * as zlib from 'zlib';
 import { TZ } from '@phensley/timezone';
-import { getSupplemental} from '../../../cldr';
+import { getSupplemental } from '../../../cldr';
 import { objectToString, Code, HEADER, NOLINT_MAXLINE } from './util';
+
+/**
+ * Split string into lines and split each line into a colon-delimited
+ * key / value pair, and convert each group into an object.
+ */
+const parseSubtagBlock = (raw: string): { [x: string]: string } => {
+  return raw.trim().split('\n').map(s => {
+    const i = s.indexOf(':');
+    return [s.substring(0, i), s.substring(i + 1)];
+  }).reduce((o: { [x: string]: string }, [k, v]) => {
+    o[k.trim()] = v.trim();
+    return o;
+  }, {});
+};
+
+const getIanaSubtags = () => {
+  const path = join(__dirname, '..', '..', '..', '..', 'data', 'raw-iana-subtags.txt.gz');
+  const raw = fs.readFileSync(path);
+  const data = zlib.gunzipSync(raw).toString('utf-8');
+  return data.split('%%').map(parseSubtagBlock).filter(r => r.Type);
+};
 
 export const getAliases = (_data: any): Code[] => {
   const supplemental = getSupplemental();
 
   const { languageAlias, scriptAlias, territoryAlias, zoneAlias } = supplemental.Aliases;
+
+  const ianaSubtags = getIanaSubtags();
+
+  const grandfathered = ianaSubtags.filter(r => r.Type === 'grandfathered').reduce((o, r) => {
+    const tag = r.Tag;
+    o[tag] = r['Preferred-Value'];
+    return o;
+  }, {});
+
+  Object.keys(grandfathered).forEach(key => {
+    if (languageAlias[key]) {
+      delete languageAlias[key];
+    }
+  });
 
   const languages = objectToString(languageAlias);
   const scripts = objectToString(scriptAlias);
