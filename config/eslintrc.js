aeb694f39283fa870c08de350e4d5913120eb1ef
Patrick Hensley <pathensley@gmail.com>
2020-04-27 17:00:41 -0400

    Add eslint global config

---
+++ config/eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  rules: {
+    "max-len": [
+      "error",
+      { code: 120 }
+    ],
+    "prefer-const": "off", // typescript compiler catches this and is more flexible
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_.*$" }
+    ],
+    "@typescript-eslint/no-use-before-define": "off",
+  },
+  settings: {
+  }
+}
+
