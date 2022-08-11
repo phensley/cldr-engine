4c1eb6e8793ef66b8c331cd43955c612f10918b3
Patrick Hensley <pathensley@gmail.com>
2022-08-11 10:00:32 -0400

    Upgrade to tz 2022b

--- packages/timezone/tztool/index.ts
+++ packages/timezone/tztool/index.ts
@@ -8,7 +8,7 @@ import { parseZIC } from './zic';
 import { encodeZones } from './encode';
 import { parseZoneTab } from './zonetab';
 
-const VERSION = '2022a';
+const VERSION = '2022b';
 
 interface JsonOptions {
   pretty: boolean;
