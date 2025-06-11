b7102fc499b04462b8f33bf2ceda67398ebb2a66
Patrick Hensley <pathensley@gmail.com>
2025-06-11 14:20:35 -0400

    Increase test timeout

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    testTimeout: 30000,
     include: ['**/__tests__/**/*.test.ts'],
     environment: 'node',
     // reporters: ['verbose'],
